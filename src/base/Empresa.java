dbb8600b83f775fabeef2164e96d25ee67708c50
Juan Jose Vallespin Millan <jjvallespin93@gmail.com>
2017-03-21 14:49:57 +0100

    Commit.

---
+++ src/base/Empresa.java
@@ -0,0 +1,26 @@
+package base;
+
+/**
+ * Created by juanj on 20/03/2017.
+ */
+public class Empresa {
+
+    private String nombre;
+    private String codigo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
